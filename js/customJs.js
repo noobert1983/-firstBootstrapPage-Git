b90dbee8caefc9903287395b9ee71558825e050d
noobert1983 <noobinprograming@gmail.com>
2018-04-23 18:18:13 +0200

    dzialajacy customJs :)
    zle sprawdzałem zmienną w if condition

--- js/customJs.js
+++ js/customJs.js
@@ -1,13 +1,42 @@
-let counter = document.getElementById("counter1").innerHTML;
+
+window.onload = function() {
+  previousSchow();
+  /*doSomethingElse();*/
+};
+
+let counter = document.getElementById("counter").innerHTML;
 let previous = document.getElementById("previous");
 console.log(counter);
 console.log(previous);
 
+function previousSchow(){
+    if(counter==1){
+    previous.innerHTML = "pierwsza strona!!!";
+    }else if(counter==2){
+    previous.innerHTML = "druga strona!!!";
+    }else{
+        previous.innerHTML = "dupa";
+    };
+}
 
+
+/*
+function previousSchow(){
 if(counter=1){
     previous.innerHTML = "pierwsza strona!!!";
-    /*previous.style.display = "none";*/
+    previous.style.display = "none";
+}else if(counter=2){
+    previous.innerHTML = "druga strona!!!";
+}else{
+    previous.innerHTML = "dupa z tego..."
 }
+};
+
+*/
+
+
+
+
 
 
 /*else if(counter>1){
@@ -18,3 +47,12 @@ if(counter=1){
     }
     previous.innerHTML = slownie +" strona!!!";
 }*/
+//
+//function mnozenie(x,y){
+//    let wynik = x*y;
+//    return wynik;
+//}
+//        
+/*console.log(mnozenie(10,2));*/
+/*let hmm = mnozenie(12,3);
+console.log(hmm);*/
